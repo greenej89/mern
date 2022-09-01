54dba76c21478893da3994d8fd84e1565a07e5af
Jessica Greene <jessica.greene89@gmail.com>
2022-09-01 12:17:44 -0400

    added prop-it-up assignment

---
+++ react/prop-it-up/src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import PersonCard from './components/PersonCard'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <PersonCard 
+          firstName={ "Jane" } 
+          lastName={ "Doe" }
+          age={ 45 }
+          hairColor={ "Black" }/>
+      </div>
+      <div>
+        <PersonCard
+          firstName={ "John" }
+          lastName={ "Smith" }
+          age={ 88 }
+          hairColor={ "Brown" }
+          />
+      </div>
+      <div>
+        <PersonCard
+          firstName={ "Millard" }
+          lastName={ "Fillmore" }
+          age={ 50 }
+          hairColor={ "Brown" }
+        />
+      </div>
+      <div>
+        <PersonCard 
+          firstName={ "Maria" }
+          lastName={ "Smith" }
+          age={ 62 }
+          hairColor={ "Brown" }
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
