a048ce9c23b931a8d8125c5a58ab10e60ff1904a
Jessica Greene <jessica.greene89@gmail.com>
2022-10-04 11:54:47 -0400

    adding product manager assignment

---
+++ full-Stack-MERN/client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import PersonForm from './components/PersonForm';
+
+function App() {
+  return (
+    <div className="App container ">
+      <PersonForm />
+    </div>
+  );
+}
+
+export default App;
